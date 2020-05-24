0aa39308beb5a4e555464d50f9065cf450d2ff02
Dan <dan.simmons@techtonicsoftware.com>
2020-05-25 00:29:58 +1000

    First full working version with different outputs and argument configs, also added a readme file.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # RCoreTester
-A script that tests the boost capacity of each individual core on the system
+A script that tests and measures the boost capacity of each individual core on the system.
+
+Techtonic Software 2020 - http://www.techtonicsoftware.com/
+
+This Program/Script Is Licensed Under GNU V3 (https://www.gnu.org/licenses/gpl-3.0.en.html) and comes with ABSOLUTELY NO WARRANTY. You may distribute, modify and run it however you must not claim it as your own nor sublisence it.
+
+Any distribution must include this readme file.
+
+This script requires the following binaries: 'lscpu' 'grep' 'sed' 'taskset' 'stress' 'bc'.
+
+I created this script in my spare time for my own personal use to help identify which cores on my Ry**n CPU have the best silicon quality and were able to boost the highest. This is useful for identifying the best performing cores for performance tuning with CPU Optomization Software (in my case my own Software RThreader (https://github.com/TechtonicSoftware/RThreader)). While I have tried to accommodate for systems with multiple CPU sockets I havn't tested it extensively on them.
+
+Please note this is a script that performs stress tests on your system using the 'stress' binary. While this is normally completely safe - by using it you do however agree to indemnify me and Techtonic Software of any and all damage that might be caused by its use to your system or otherwise (e.g. overheating CPU, etc).
+
+Usage: ./command args
+	none: Operate in default test mode.
+    --help: Show this dialog.
+    -s: Silent mode - do not show intro, ask to start test and only output results.
+    -vc: Virtual Core Mode - Output all results as their virtual core/thread value on the system.
+    -csv: CSV Mode - Output final results on one line seperated by commas.
--- rcoretester.sh
+++ rcoretester.sh
@@ -3,7 +3,20 @@
 #STATICVALS
 REQBINS=('lscpu' 'grep' 'sed' 'taskset' 'stress' 'bc');
 SPACERSTR="\n------------------------------\n";
-TESTTIMEPERCORE_SEC=5;
+HELPSTR="
+    Usage: ./command args
+	none: Operate in default test mode.
+    --help: Show this dialog.
+    -s: Silent mode - do not show intro, ask to start test and only output results.
+    -vc: Virtual Core Mode - Output all results as their virtual core/thread value on the system.
+    -csv: CSV Mode - Output final results on one line seperated by commas.
+";
+TESTTIMEPERCORE_SEC=60;
+
+#Arg Vals
+SILENTMODE=0;
+VIRTCOREMODE=0;
+CSVMODE=0;
 
 #Vals
 CPUDETAILS=();
@@ -28,6 +41,8 @@ CORE_AVERAGEFREQ=0;
 FINAL_RESULTSPOS=();
 FINAL_AVERAGEFREQS=();
 FINAL_HIGHESTFREQS=();
+FINAL_RESULTSSTR="";
+TEMPVAL="";
 
 #Check all required binaries are present
 for bin in "${REQBINS[@]}"; do
@@ -35,6 +50,27 @@ for bin in "${REQBINS[@]}"; do
 		echo "Missing required executable $bin! Please ensure the application is installed before running this script.";
 		exit 1;
 	fi
+done
+
+#Loop Through Arguments
+for arg in "${@}"; do
+
+    case $arg in
+        "-s")
+            SILENTMODE=1;;
+        "-vc")
+            VIRTCOREMODE=1;;
+        "-csv")
+            CSVMODE=1;;
+        "--help")
+            echo -e "$HELPSTR";
+            exit 0;;
+        *)
+            echo -e "Unknown Argument: $arg.";
+            echo -e "$HELPSTR";
+            exit 1;;
+    esac
+
 done
 
 #Get Number of Actual Cores and sockets as well as work out a time estimate for the script to complete
@@ -46,25 +82,29 @@ NUMTHREADS=$(( ($CORESPERSOCKET*$NUMSOCKETS)*$THREADSPERCORE ));
 TIMEESTIMATE=$(( $TESTTIMEPERCORE_SEC*($NUMSOCKETS*$CORESPERSOCKET) ));
 TIMEESTIMATE_MINUTES=$(( $TIMEESTIMATE/60 )); #Yes I know its not precise due to truncation, its only an estimate..
 
-echo "Found $NUMSOCKETS socket/s with $CORESPERSOCKET core/s per socket and $NUMTHREADS thread/s.";
 
-#Announce some stuff and ask if ok to proceed
-echo -e $SPACERSTR;
-echo "Welcome to RCoreTester!";
-echo "This script will now test each of your core/s boost clock capacity." \
-    "Please note that if you do not have frequency boost/hyperthreading enabled (or indeed a cpu that can boost per-core) this script is rather pointless - so make sure it is enabled in bios.";
-echo "Please also avoid running any additional stress tests while this script is running as it may cause strange results.";
+#If silent mode running skip intro+confirm
+if [ "$SILENTMODE" -ne 1 ]; then
+    echo "Found $NUMSOCKETS socket/s with $CORESPERSOCKET core/s per socket and $NUMTHREADS thread/s.";
 
-echo -e "\nBased on the number of cores you have this script will take around $TIMEESTIMATE_MINUTES minutes to run.";
-echo -e "Ok to proceed (y/n)?";
-read USERINPUT;
+    #Announce some stuff and ask if ok to proceed
+    echo -e $SPACERSTR;
+    echo "Welcome to RCoreTester!";
+    echo "This script will now test each of your core/s boost clock capacity." \
+        "Please note that if you do not have frequency boost/hyperthreading enabled (or indeed a cpu that can boost per-core) this script is rather pointless - so make sure it is enabled in bios.";
+    echo "Please also avoid running any additional stress tests while this script is running as it may cause strange results.";
 
-if [ "$USERINPUT" != "y" ]; then
-    exit 0;
+    echo -e "\nBased on the number of cores you have this script will take around $TIMEESTIMATE_MINUTES minutes to run.";
+    echo -e "Ok to proceed (y/n)?";
+    read USERINPUT;
+
+    if [ "$USERINPUT" != "y" ]; then
+        exit 0;
+    fi
+
+    echo -e $SPACERSTR;
+    echo "Running test..";
 fi
-
-echo -e $SPACERSTR;
-echo "Running test..";
 
 COUNT_SOCKETS=0;
 
@@ -78,7 +118,9 @@ while [ $COUNT_SOCKETS -lt $NUMSOCKETS ]; do
     #Loop Through this sockets cores
     COUNT=$STARTPOS;
     while [ $COUNT -lt $ENDPOS ]; do
-        echo "Testing socket $COUNT_SOCKETS, core $COUNT";
+        if [ "$SILENTMODE" -ne 1 ]; then
+            echo "Testing socket $COUNT_SOCKETS, core $COUNT";
+        fi
 
         #Reset corefreq array and maxFreq
         unset CORE_FREQS;
@@ -111,8 +153,14 @@ while [ $COUNT_SOCKETS -lt $NUMSOCKETS ]; do
         CORE_AVERAGEFREQ=$(echo "${CORE_FREQS[@]}" | sed 's/ /+/g' | bc);
         CORE_AVERAGEFREQ=$(echo "$CORE_AVERAGEFREQ/${#CORE_FREQS[@]}" | bc);
 
+        #If virtual core mode add virtual core, otherwise socket:core
+        if [ "$VIRTCOREMODE" -eq 1 ]; then
+            FINAL_RESULTSPOS+=("$COUNT");
+        else
+            FINAL_RESULTSPOS+=("$COUNT_SOCKETS:$((COUNT-STARTPOS))");
+        fi
+
         #Add to results array
-        FINAL_RESULTSPOS+=("$COUNT_SOCKETS:$COUNT");
         FINAL_AVERAGEFREQS+=($CORE_AVERAGEFREQ);
         FINAL_HIGHESTFREQS+=($CORE_MAXFREQ);
 
@@ -122,13 +170,43 @@ while [ $COUNT_SOCKETS -lt $NUMSOCKETS ]; do
     COUNT_SOCKETS=$(( $COUNT_SOCKETS+1 ));
 done
 
-#Present Results
-echo -e "\nFinal Results (Socket:Core): ";
-COUNT=0;
-ENDPOS="${#FINAL_AVERAGEFREQS[@]}";
-while [ $COUNT -lt $ENDPOS ]; do
+#Bubble Sort (very inefficent so may change later)
+ENDPOS=$((ENDPOS-1));
+for ((i=0; i<ENDPOS; i++)); do
+    for ((j=0; j<ENDPOS; j++)); do
+        if (( $(echo "${FINAL_AVERAGEFREQS[$((j+1))]} > ${FINAL_AVERAGEFREQS[$j]}" | bc -l) )); then
 
-    echo "${FINAL_RESULTSPOS[$COUNT]} - High of ${FINAL_HIGHESTFREQS[$COUNT]} Mhz, ${FINAL_AVERAGEFREQS[$COUNT]} Mhz average";
+            #Average Freqs:
+            TEMPVAL="${FINAL_AVERAGEFREQS[$((j+1))]}";
+            FINAL_AVERAGEFREQS[$((j+1))]="${FINAL_AVERAGEFREQS[$j]}";
+            FINAL_AVERAGEFREQS[$j]="$TEMPVAL";
 
-    COUNT=$(( $COUNT+1 ));
+            #Result Pos
+            TEMPVAL="${FINAL_RESULTSPOS[$((j+1))]}";
+            FINAL_RESULTSPOS[$((j+1))]="${FINAL_RESULTSPOS[$j]}";
+            FINAL_RESULTSPOS[$j]="$TEMPVAL";
+        fi
+    done
 done
+
+#If csv mode set output as csv, otherwise 
+if [ "$CSVMODE" -eq 1 ]; then
+    FINAL_RESULTSSTR=$(echo "${FINAL_RESULTSPOS[@]}" | sed 's/ /,/g');
+else
+    FINAL_RESULTSSTR="Best cores in order (Socket:Core): ${FINAL_RESULTSPOS[@]}";
+fi
+
+#If not silent mode output more details
+if [ "$SILENTMODE" -ne 1 ]; then
+    echo -e "\nFinal Results (Socket:Core): ";
+    COUNT=0;
+    ENDPOS="${#FINAL_AVERAGEFREQS[@]}";
+    while [ $COUNT -lt $ENDPOS ]; do
+
+        echo "${FINAL_RESULTSPOS[$COUNT]} - High of ${FINAL_HIGHESTFREQS[$COUNT]} Mhz, ${FINAL_AVERAGEFREQS[$COUNT]} Mhz average";
+
+        COUNT=$(( $COUNT+1 ));
+    done
+fi
+
+echo $FINAL_RESULTSSTR;
